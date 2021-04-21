aabefc4a53b28f214c4ae5e3604092d3c8f36e22
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-04-20 19:55:24 -0700

    Switch to a BERT-finetuning approach

--- model.py
+++ model.py
@@ -4,18 +4,25 @@ import torch as th
 from torch import nn
 from torch.nn.modules import rnn
 
+from transformers import DistilBertForSequenceClassification
+
 # Using a basic RNN/LSTM for Language modeling
 class ReviewPredictionModel(nn.Module):
     def __init__(self, vocab_size, rnn_size, num_layers=1, dropout=0):
         super().__init__()
-        self.rnn_size = rnn_size
-        self.embedding = nn.Embedding(vocab_size, rnn_size)
-        self.lstm = nn.LSTM(input_size=rnn_size, hidden_size=rnn_size, num_layers=num_layers, bidirectional=True, batch_first=True)
-        self.linear = nn.Linear(rnn_size * 2, 1)
+        # self.rnn_size = rnn_size
+        # self.embedding = nn.Embedding(vocab_size, rnn_size)
+        # self.lstm = nn.LSTM(input_size=rnn_size, hidden_size=rnn_size, num_layers=num_layers, bidirectional=True, batch_first=True)
+        # self.linear = nn.Linear(rnn_size * 2, 1)
+        self.transfomer = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
         
-    def forward(self,x):
-        embedded_input = self.embedding(x)
-        through_lstm,  _ = self.lstm(embedded_input)
-        lstm_forward_last = through_lstm[:,-1,:self.rnn_size]
-        lstm_backward_last = through_lstm[:,0,self.rnn_size:]
-        return th.sigmoid(self.linear(th.cat((lstm_forward_last, lstm_backward_last), dim=1))) * 4 + 1 # soft clipping to the review range
+    def forward(self,x,x_mask):
+        x = x[:,:64]
+        x_mask = x_mask[:,:64]
+        transfomer_logit = self.transfomer(input_ids=x, attention_mask=x_mask, return_dict=True)["logits"][:,0]
+
+        # embedded_input = self.embedding(x)
+        # through_lstm,  _ = self.lstm(embedded_input)
+        # lstm_forward_last = through_lstm[:,-1,:self.rnn_size]
+        # lstm_backward_last = through_lstm[:,0,self.rnn_size:]
+        return th.sigmoid(transfomer_logit) * 4 + 1 # soft clipping to the review range
--- training_utils.py
+++ training_utils.py
@@ -15,8 +15,9 @@ def split_train_validation(source_data: List[T], val_percent: float) -> Tuple[Li
 def pad_text(text: List[int], to_length: int, with_index: int) -> List[int]:
   return text + [with_index] * (to_length - len(text))
 
-batch_to_torch = lambda b_in,b_target: (th.tensor(b_in, dtype=th.long),
-                                        th.tensor(b_target, dtype=th.float))
+batch_to_torch = lambda b_in,b_target,b_mask: (th.tensor(b_in, dtype=th.long),
+                                        th.tensor(b_target, dtype=th.float),
+                                        th.tensor(b_mask, dtype=th.float))
 
 def list_to_device(all_tensors, device):
   return [tensor.to(device) for tensor in all_tensors]
@@ -25,14 +26,13 @@ Example = Tuple[str, float]
 def run_training_loop(
   model, optimizer, device,
   batch_size: int, epochs: int,
-  train_x: List[str], train_y: List[float],
-  validation_x: List[str], validation_y: List[float],
+  train_x: List[str], train_mask, train_y: List[float],
+  validation_x: List[str], validation_mask, validation_y: List[float],
   model_id: str = "experiment"
 ):
-  (validation_batch_input, validation_batch_target) = batch_to_torch(validation_x, validation_y)
-  (validation_batch_input, validation_batch_target) = list_to_device((validation_batch_input, validation_batch_target), device)
+  (validation_batch_input, validation_batch_target, validation_batch_mask) = batch_to_torch(validation_x[:64,:], validation_y[:64], validation_mask[:64])
+  (validation_batch_input, validation_batch_target, validation_batch_mask) = list_to_device((validation_batch_input, validation_batch_target, validation_batch_mask), device)
 
-  # print(validation_batch_target)
   loss_fn = th.nn.MSELoss()
   model.train()
   losses = []
@@ -43,14 +43,15 @@ def run_training_loop(
       t = tqdm(range(0,(len(train_x)//batch_size)+1))
       for i in t:
           batch_indices = indices[i*batch_size:(i+1)*batch_size]
-          (batch_input, batch_target) = (
+          (batch_input, batch_target, batch_mask) = (
             train_x[batch_indices],
-            train_y[batch_indices]
+            train_y[batch_indices],
+            train_mask[batch_indices]
           )
-          (batch_input, batch_target) = batch_to_torch(batch_input, batch_target)
-          (batch_input, batch_target) = list_to_device((batch_input, batch_target), device)
+          (batch_input, batch_target, batch_mask) = batch_to_torch(batch_input, batch_target, batch_mask)
+          (batch_input, batch_target, batch_mask) = list_to_device((batch_input, batch_target, batch_mask), device)
           
-          prediction = th.flatten(model(batch_input))
+          prediction = th.flatten(model(batch_input, batch_mask))
           loss = loss_fn(prediction, batch_target)
           losses.append(loss.item())
           accuracy = th.mean(th.eq(th.round(prediction * 2), th.round(batch_target * 2)).float())
@@ -61,7 +62,7 @@ def run_training_loop(
           optimizer.step()
           if i % 10 == 0:
               model.eval()
-              validation_prediction = th.flatten(model(validation_batch_input))
+              validation_prediction = th.flatten(model(validation_batch_input, validation_batch_mask))
               validation_loss = loss_fn(validation_prediction, validation_batch_target).item()
               # print(validation_prediction)
               validation_accuracy = th.mean(th.eq(th.round(validation_prediction * 2), th.round(validation_batch_target * 2)).float())
